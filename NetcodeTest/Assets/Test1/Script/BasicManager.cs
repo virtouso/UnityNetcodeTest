bf439f3aadb2ef35a0b18a8e79424602ddc8853d
virtouso <s_moeen_kamali@yahoo.com>
2023-08-12 12:15:15 +0330

    currently nothing

--- NetcodeTest/Assets/Test1/Script/BasicManager.cs
+++ NetcodeTest/Assets/Test1/Script/BasicManager.cs
@@ -1,5 +1,6 @@
-using System;
+
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +10,19 @@ namespace Test1.Script
     {
 
         [SerializeField] private Button clientButton;
-        
-        
+        [SerializeField] private UnityTransport transport;
+        [SerializeField] private bool debug;
         private void Start()
         {
             clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+
+            if (debug)
+            {
+                transport.SetDebugSimulatorParameters(packetDelay:120, packetJitter:5, dropRate:3);
+            }
+            
+            
+            
         }
     }
 }
